5838fdbc48fd1a8342dc2c71ce539c2f7e0376e3
Skidconnection <44195788+Skidconnection@users.noreply.github.com>
2021-06-08 09:00:19 +0200

    Alles toevegoegd

---
+++ result.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="NL" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style/page2-style.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <a href=""><button type="button" name="button">Klik hier om je resultaat te zien</button></a>
+    </div>
+  </body>
+</html>
